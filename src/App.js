842a7fbdb215fbc48c5cf8673fc9511f11a5fcc2
mike0z <mikeoz@grdwrx.com>
2023-12-26 16:59:27 -0600

    start over

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import PersonForm from './PersonForm';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Edit <code>src/App.js</code> or just display this for success.</p>
-          <Link to="/person-form" className="App-link">Go to Person Form</Link>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-        </header>
-        <Route path="/person-form" component={PersonForm} />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> or just display this for success.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
