7f732f2724e5dc690ae887483c6702a2830d193e
mike0z <mikeoz@grdwrx.com>
2024-01-02 17:26:09 -0600

    Test Link

--- src/TestPage.js
+++ src/TestPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 function TestPage() {
   return (
     <div>
       <h1>This is the Test Page.  Deployed 02JAN24_v1</h1>
       <p>Welcome to your new page!</p>
+      {/* Add a Link to navigate back to the main page */}
+      <Link to="/" className="App-link">
+        Return to Main Page
+      </Link>
     </div>
   );
 }
