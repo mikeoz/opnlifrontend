8dac268eda6683d13532627d6a6b80414cb1e26c
mike0z <mikeoz@grdwrx.com>
2024-01-02 16:02:39 -0600

    Add Test Page

---
+++ src/TestPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TestPage() {
+  return (
+    <div>
+      <h1>This is the Test Page</h1>
+      <p>Welcome to your new page!</p>
+    </div>
+  );
+}
+
+export default TestPage;
