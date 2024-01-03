5c3fde5cd1abe704cf2e6fb4849a9cd7e8fe7882
mike0z <mikeoz@grdwrx.com>
2024-01-03 16:25:59 -0600

    PF Navigation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+A new Person Form has been created in this React.js project and version Opnli_Crash_5_OpnliFrontEnd_PC-12-1_PersonForm_Baseline_03JAN24.txt contains the current code, which is shown below.
+
+The task is to:
+
+1.  Modify the code in App.js so that the Person Form page is displayed when the link for First Page is selected;
+
+2.  Modify the code in the PersonForm.js file to add a link that returns the user to the Home page when it is selected; and 
+
+3.  Write a short tutorial explaining to a Beginner Developer the changes that were made and why they will be effective in accomplishing these purposes.
+
+The current App.js code is as follows:
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
@@ -32,3 +44,22 @@ const SecondPage = () => (
 );
 
 export default App;
+
+
+The current PersonForm.js code is as follows:
+
+// PersonForm.js
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('/api/addPerson', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: personName, age: personAge }),
+    });
+  
+    const data = await response.json();
+    console.log(data);
+  };
+  
