d6e0455cd6b9d79633f9e6a2d625cf6b37ed3751
mike0z <mikeoz@grdwrx.com>
2024-01-03 08:49:12 -0600

    Fix Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom'; // Import Router and Link
-import TestPage from './TestPage'; // Make sure you have this component created
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; // Import Route along with Router and Link
+import TestPage from './TestPage'; // Import the TestPage component
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Router> {/* Wrap the content in a Router */}
+    <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -21,11 +21,15 @@ function App() {
           >
             Learn React
           </a>
-          {/* Add the new Link for TestPage */}
-          <Link to="/test-page" className="App-link">
-            Test Page
-          </Link>
+          <Link to="/test-page" className="App-link">Test Page</Link> {/* Link to TestPage */}
         </header>
+        {/* Routes define the components to render for specific paths */}
+        <Route exact path="/" render={() => (
+          <React.Fragment>
+            {/* Content of the main page */}
+          </React.Fragment>
+        )}/>
+        <Route path="/test-page" component={TestPage} /> {/* Route to TestPage */}
       </div>
     </Router>
   );
